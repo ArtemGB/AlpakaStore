7c28ae3429eee44890c582006d2f110f7ed89bb5
ArtemGG <gmadrich@gmail.com>
2021-01-15 16:37:02 +0300

    Added EntityFrameworkCore

---
+++ Core/DbControl/StoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Core.DbControl
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+
+        }
+
+        private string GetConnectionStringFromJSON()
+        {
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath($"../{Directory.GetCurrentDirectory()}");
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            return config.GetConnectionString("DefaultConnection");
+        }
+    }
+}
